c2154bb6a5b8037dace65c6c04cc13e0a80d4018
lhscaldas <lhscaldas@gmail.com>
2025-04-04 15:32:11 -0300

    feito até a questão 5 (3 e 4 estão no caderno)

---
+++ Lista_2/q5.py
@@ -0,0 +1,35 @@
+from random import random
+import matplotlib.pyplot as plt
+
+def amostra_V():
+    soma = 0
+    contador = 0
+    while soma < 1:
+        x = random()
+        soma += x
+        contador += 1
+    return contador
+
+def estimador_E(n):
+    soma = 0
+    for i in range(n):
+        x = amostra_V()
+        soma += x
+    return soma / n
+
+def grafico_E(n_max):
+    
+    n_values = list(range(1, n_max + 1))
+    E_values = [estimador_E(n) for n in n_values]
+
+    plt.plot(n_values, E_values, label='E[V] a cada n')
+    label = f'Limite E[V] = {E_values[-1]}'
+    plt.axhline(y=E_values[-1], color='r', linestyle='--', label=label)
+    plt.title('Estimador de E[V] em função de n')
+    plt.xlabel('n')
+    plt.ylabel('E[V]')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+grafico_E(10000)
